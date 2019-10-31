620176b6ee666f25c3a33cf76e36229d78d14952
Joe <josephc.lambert@gmail.com>
2019-10-31 11:58:45 -0700

    feature: validateUser and validateUserId middlewares meet spec and are applied to POST /:id/posts

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const userDb = require('./userDb.js');
+const postDb = require('../posts/postDb.js');
 const isEmpty = require('../utils/isEmpty.js');
 
 const router = express.Router();
-router.use(validateUser);
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   const { name } = req.body;
   userDb
     .insert({ name })
@@ -19,7 +19,22 @@ router.post('/', (req, res) => {
     });
 });
 
-router.post('/:id/posts', (req, res) => {});
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const { user } = req;
+  const post = {
+    text: req.body.text,
+    user_id: user.id
+  };
+  postDb
+    .insert(post)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(error => {
+      console.log('Create new post error: ', error);
+      res.status(500).json({ message: 'Error saving new post to database' });
+    });
+});
 
 router.get('/', (req, res) => {});
 
@@ -33,7 +48,25 @@ router.put('/:id', (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {}
+function validateUserId(req, res, next) {
+  const { id } = req.params;
+
+  userDb
+    .getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: 'invalid user id' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: 'There was an error retrieving that user from the database'
+      });
+    });
+}
 
 function validateUser(req, res, next) {
   const { name } = req.body;
@@ -51,6 +84,15 @@ function validateUser(req, res, next) {
   next();
 }
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+  const post = req.body;
+  if (isEmpty(post)) {
+    res.status(400).json({ message: 'missing post data' });
+  } else if (!post.text) {
+    res.status(400).json({ message: 'missing required text field' });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
