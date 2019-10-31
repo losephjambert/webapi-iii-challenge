45c600eee6abe8694df34a4f572867c6b35f3ff5
Joe <josephc.lambert@gmail.com>
2019-10-31 08:23:27 -0700

    servers runs on localhost

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const server = require('./server');
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`server now coming to you live from port ${PORT}`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = 'express';
+const express = require('express');
+const postRouter = require('./posts/postRouter.js');
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
+server.use(express.json());
+
+server.use('/api/post', postRouter);
+
+server.use('/api/user', userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
